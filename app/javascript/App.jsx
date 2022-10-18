680fd9afe3547b908bbf5dca49da3d5606e0869d
Александр Сдобников <sdobnikove@mail.ru>
2022-10-18 22:05:06 +0300

    add code loading from snippet when snippet search params are present #5

--- app/javascript/App.jsx
+++ app/javascript/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { MonacoEditor } from './components/Editor/index.jsx';
 import { Button } from './components/Button/index.jsx';
 import { Terminal } from './components/Terminal/index.jsx';
+import { actions } from './slices/index.js';
+import { useDispatch } from 'react-redux';
+import { useSnippets } from './hooks';
 
 export function App() {
+  const dispatch = useDispatch();
+  const snippetApi = useSnippets();
+
+  useEffect( () => {
+    const loadSnippet = async () => {
+      if (snippetApi.hasSnippetParams()) {
+        const decodedId = snippetApi.getSnippetIdFromParams();
+        const snippetData = await snippetApi.getSnippetData(decodedId);
+        dispatch(actions.updateCode(snippetData.code));
+      }
+    };
+    loadSnippet();
+  });
+
   return (
     <main className="container-fluid my-5">
       <div className="row mb-4">
