2cd6c0c43577be4267a8e718d3ce29360217f480
Александр Сдобников <sdobnikove@mail.ru>
2022-10-18 22:00:34 +0300

    add snippet provider component as a context api unified interface for operating on snippets #5

--- app/javascript/contexts/index.js
+++ app/javascript/contexts/index.js
@@ -1,3 +1,5 @@
 import { createContext } from 'react';
 
 export const AuthContext = createContext({});
+
+export const SnippetsContext = createContext({});
--- app/javascript/hooks/index.js
+++ app/javascript/hooks/index.js
@@ -1,5 +1,8 @@
 import { useContext } from 'react';
 
 import { AuthContext } from '../contexts/index.js';
+import { SnippetsContext } from '../contexts/index.js';
 
 export const useAuth = () => useContext(AuthContext);
+export const useSnippets = () => useContext(SnippetsContext);
+
