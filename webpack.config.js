c6d4f74214136f4587939c817457c2ec49ca8c06
antonkrupin <krupinanton87@gmail.com>
2022-12-15 16:05:28 +0300

    change webpack config before change i have error in ChromeDev tools: uncaught (in promise) referenceerror: process is not defined at __webpack_default_export__
    with this error i see only landing page, links dosen`t works
    only black page

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const webpack = require('webpack')
 // const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 const env = process.env.NODE_ENV || 'development';
@@ -82,5 +83,8 @@ module.exports = {
     new MiniCssExtractPlugin(),
     // Add your plugins here
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+		new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
   ],
 };
