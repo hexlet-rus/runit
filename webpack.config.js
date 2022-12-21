765c883c9c89e38956bea23147a4424d05548d0f
antonkrupin <krupinanton87@gmail.com>
2022-12-21 11:24:11 +0300

    remove changes in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const webpack = require('webpack')
 // const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 const env = process.env.NODE_ENV || 'development';
@@ -83,8 +82,5 @@ module.exports = {
     new MiniCssExtractPlugin(),
     // Add your plugins here
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-		new webpack.ProvidePlugin({
-      process: 'process/browser',
-    }),
   ],
 };
