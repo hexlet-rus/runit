52024d151a65403ff1c2e9cc5beebe498894e098
Александр Сдобников <sdobnikove@mail.ru>
2023-02-03 19:46:35 +0300

    add correct Strategy local import and "jwt" naming

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -1,11 +1,11 @@
 /* eslint-disable class-methods-use-this */
-import { Strategy } from 'passport-jwt';
+import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import getJwtExtractConfig from './config/jwt-extract.config';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor() {
     super(getJwtExtractConfig());
   }
