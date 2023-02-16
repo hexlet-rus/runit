65967d83621c9d95fa30a77b119194243c09b8f1
AnastasiaKv <anastasia.kvartalna@gmail.com>
2023-02-16 23:33:18 +0300

    Fetch user data for generation of sharing links

--- frontend/src/components/Editor/index.jsx
+++ frontend/src/components/Editor/index.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Editor from '@monaco-editor/react';
 
 import { useTranslation } from 'react-i18next';
 import { useEditor } from './hooks.js';
 import { useAuth } from '../../hooks';
+import { useDispatch } from 'react-redux';
 import classes from './Editor.module.css';
+import { fetchData } from '../../slices/userSlice.js';
 
 function AuthBanner() {
   const { t } = useTranslation();
@@ -20,12 +22,23 @@ function AuthBanner() {
 export function MonacoEditor() {
   const { code, language, onChange } = useEditor();
   const { isLoggedIn } = useAuth();
+  const dispatch = useDispatch();
 
   const options = {
     selectOnLineNumbers: true,
     wordWrap: true,
     readOnly: !isLoggedIn,
   };
+
+  useEffect(() => {
+    dispatch(fetchData())
+      .unwrap()
+      .catch((serializedError) => {
+        const error = new Error(serializedError.message);
+        error.name = serializedError.name;
+        throw error;
+      });
+  }, []);
 
   return (
     <div className={classes.wrapper}>
