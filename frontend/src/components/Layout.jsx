e433fcbd105e9c7b29044071cec0cc3c37158e43
Masha <nasonkina@yandex.ru>
2023-04-23 04:39:37 +0300

    fix navbar (add react-bootstrap components, fix responsive behaviour)

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import { Navbar } from './Navbar.jsx';
+import { NavigationBar } from './Navbar.jsx';
 import { buildFooter as Footer } from './Footer.jsx';
 
 function Layout() {
   return (
     <>
-      <Navbar />
+      <NavigationBar />
       <Outlet />
       <Footer />
     </>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,61 +1,97 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
 import { useTranslation } from 'react-i18next';
 import routes from '../routes.js';
 import { useAuth } from '../hooks';
 import Logo from '../assets/landing/images/logo.svg';
 
-export function Navbar() {
+export function NavigationBar() {
   const auth = useAuth();
   const { t } = useTranslation();
 
   return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-dark text-white shadow-sm border-bottom-mb-5">
-      <div className="container">
-        <Link
-          className="d-flex navbar-brand"
+    <Navbar
+      variant="dark"
+      bg="dark"
+      expand="lg"
+      className="shadow-sm border-bottom-mb-5"
+    >
+      <Container>
+        <Navbar.Brand
+          as={Link}
+          className="d-flex"
           aria-hidden="true"
-          to={routes.homePagePath()}
+          href={routes.homePagePath()}
         >
-          <img
+          <Image
             alt="mainLabel"
             width={100}
             height={30}
             src={Logo}
             className="pb-1"
           />
-          <span className="ms-1">{t('navbar.mainLabel')}</span>
-        </Link>
-        <div className="navbar-nav justify-content-end">
-          {auth.isLoggedIn && (
-            <Link className="nav-link px-3" to={routes.profilePagePath()}>
-              {t('navbar.profile')}
-            </Link>
-          )}
-          <Link className="nav-link px-3" to={routes.aboutPagePath()}>
-            {t('navbar.about')}
-          </Link>
-          {auth.isLoggedIn && (
-            <button
-              type="button"
-              className="btn nav-link px-3"
-              onClick={auth.logOut}
-            >
-              {t('navbar.logout')}
-            </button>
-          )}
-          {!auth.isLoggedIn && (
-            <Link className="nav-link px-3" to={routes.loginPagePath()}>
-              {t('navbar.signIn')}
-            </Link>
-          )}
-          {!auth.isLoggedIn && (
-            <Link className="nav-link px-3" to={routes.signUpPagePath()}>
-              {t('navbar.signUp')}
-            </Link>
-          )}
-        </div>
-      </div>
-    </nav>
+          <Navbar.Text className="ms-1">{t('navbar.mainLabel')}</Navbar.Text>
+        </Navbar.Brand>
+        <Navbar.Toggle as="button" aria-controls="navbar-toggler" />
+        <Navbar.Collapse id="navbar-toggler">
+          <Nav as="ul" className="d-flex justify-content-end">
+            {auth.isLoggedIn && (
+              <Nav.Item as="li">
+                <Nav.Link
+                  as={Link}
+                  className="nav-link px-3"
+                  to={routes.profilePagePath()}
+                >
+                  {t('navbar.profile')}
+                </Nav.Link>
+              </Nav.Item>
+            )}
+            <Nav.Item as="li">
+              <Nav.Link
+                as={Link}
+                className="nav-link px-3"
+                to={routes.aboutPagePath()}
+              >
+                {t('navbar.about')}
+              </Nav.Link>
+            </Nav.Item>
+            {auth.isLoggedIn && (
+              <Nav.Item as="li">
+                <Button type="button" className="px-3" onClick={auth.logOut}>
+                  {t('navbar.logout')}
+                </Button>
+              </Nav.Item>
+            )}
+            {!auth.isLoggedIn && (
+              <Nav.Item as="li">
+                <Nav.Link
+                  as={Link}
+                  className="px-3"
+                  to={routes.loginPagePath()}
+                >
+                  {t('navbar.signIn')}
+                </Nav.Link>
+              </Nav.Item>
+            )}
+            {!auth.isLoggedIn && (
+              <Nav.Item as="li">
+                <Nav.Link
+                  as={Link}
+                  className="px-3"
+                  to={routes.signUpPagePath()}
+                >
+                  {t('navbar.signUp')}
+                </Nav.Link>
+              </Nav.Item>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
