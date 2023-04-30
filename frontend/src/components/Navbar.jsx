c9712e68c90432d0b24bac5b452697e78dcbf637
Masha <nasonkina@yandex.ru>
2023-05-01 01:31:35 +0300

    fix Navbar styles and logo (remove blank space from svg), add header tag

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ import Button from 'react-bootstrap/Button';
 import { useTranslation } from 'react-i18next';
 import routes from '../routes.js';
 import { useAuth } from '../hooks';
-import Logo from '../assets/landing/images/logo.svg';
+import Logo from '../assets/landing/images/logo1.svg';
 import classes from './Navbar.module.css';
 
 export function Navbar() {
@@ -16,90 +16,105 @@ export function Navbar() {
   const { t } = useTranslation();
 
   return (
-    <NavigationBar
-      variant="dark"
-      bg="dark"
-      expand="md"
-      className="border-bottom-mb-5"
-    >
-      <Container>
-        <NavigationBar.Brand
-          as={Link}
-          className="d-flex align-items-baseline"
-          to={routes.homePagePath()}
-        >
-          <Image
-            alt={t('navbar.mainLabel')}
-            width={113}
-            height={24}
-            src={Logo}
-            aria-hidden="true"
-            className={`${classes.navbarLogo}`}
-          />{' '}
-          <span className={`${classes.navbarText} d-block ms-2 text-white`}>
-            {t('navbar.mainLabel')}
-          </span>
-        </NavigationBar.Brand>
-        <NavigationBar.Toggle as="button" aria-controls="navbar-toggler" />
-        <NavigationBar.Collapse
-          id="navbar-toggler"
-          className="justify-content-end"
-        >
-          <Nav as="ul">
-            {auth.isLoggedIn && (
+    <header>
+      <NavigationBar
+        variant="dark"
+        bg="dark"
+        expand="md"
+        className="border-bottom-mb-5"
+      >
+        <Container>
+          <NavigationBar.Brand
+            as={Link}
+            className="d-flex align-items-baseline"
+            to={routes.homePagePath()}
+          >
+            <Image
+              alt={t('navbar.mainLabel')}
+              width={113}
+              height={24}
+              src={Logo}
+              aria-hidden="true"
+              className={`${classes.navbarLogo}`}
+            />{' '}
+            <span className={`${classes.navbarText} d-block ms-2 text-white`}>
+              {t('navbar.mainLabel')}
+            </span>
+          </NavigationBar.Brand>
+          <NavigationBar.Toggle as="button" aria-controls="navbar-toggler" />
+          <NavigationBar.Collapse
+            id="navbar-toggler"
+            className="justify-content-end"
+          >
+            <Nav as="ul">
+              {auth.isLoggedIn && (
+                <Nav.Item as="li" className="d-flex align-items-center px-2">
+                  <Nav.Link
+                    as={Link}
+                    to={routes.profilePagePath()}
+                    className={`${classes.navLink}`}
+                  >
+                    {t('navbar.profile')}
+                  </Nav.Link>
+                </Nav.Item>
+              )}
               <Nav.Item as="li" className="d-flex align-items-center px-2">
                 <Nav.Link
                   as={Link}
-                  to={routes.profilePagePath()}
-                  className={`${classes.navLink} ${classes.lh6}`}
+                  to={routes.aboutPagePath()}
+                  className={`${classes.navLink}`}
                 >
-                  {t('navbar.profile')}
+                  {t('navbar.about')}
                 </Nav.Link>
               </Nav.Item>
-            )}
-            <Nav.Item as="li" className="d-flex align-items-center px-2">
-              <Nav.Link
-                as={Link}
-                to={routes.aboutPagePath()}
-                className={`${classes.navLink} ${classes.lh6}`}
-              >
-                {t('navbar.about')}
-              </Nav.Link>
-            </Nav.Item>
-            {auth.isLoggedIn && (
-              <Nav.Item as="li" className="py-2 px-2 d-flex align-items-center">
-                <Button variant="primary" onClick={auth.logOut}>
-                  {t('navbar.logout')}
-                </Button>
-              </Nav.Item>
-            )}
-            {!auth.isLoggedIn && (
-              <Nav.Item as="li" className="py-2 px-2 d-flex align-items-center">
-                <Button
-                  variant="light"
-                  as={Link}
-                  to={routes.loginPagePath()}
-                  className={`${classes.btnLight}`}
+              {auth.isLoggedIn && (
+                <Nav.Item
+                  as="li"
+                  className="py-2 px-2 d-flex align-items-center"
                 >
-                  {t('navbar.signIn')}
-                </Button>
-              </Nav.Item>
-            )}
-            {!auth.isLoggedIn && (
-              <Nav.Item as="li" className="py-2 px-2 d-flex align-items-center">
-                <Button
-                  variant="primary"
-                  as={Link}
-                  to={routes.signUpPagePath()}
-                  className={`${classes.btnPrimary}`}
+                  <Button
+                    variant="primary"
+                    className={`${classes.btnPrimary}`}
+                    onClick={auth.logOut}
+                  >
+                    {t('navbar.logout')}
+                  </Button>
+                </Nav.Item>
+              )}
+              {!auth.isLoggedIn && (
+                <Nav.Item
+                  as="li"
+                  className="py-2 px-2 d-flex align-items-center"
                 >
-                  {t('navbar.signUp')}
-                </Button>
-              </Nav.Item>
-            )}
-          </Nav>
-        </NavigationBar.Collapse>
-      </Container>
-    </NavigationBar>
+                  <Button
+                    variant="light"
+                    as={Link}
+                    to={routes.loginPagePath()}
+                    className={`${classes.btnLight}`}
+                  >
+                    {t('navbar.signIn')}
+                  </Button>
+                </Nav.Item>
+              )}
+              {!auth.isLoggedIn && (
+                <Nav.Item
+                  as="li"
+                  className="py-2 px-2 d-flex align-items-center"
+                >
+                  <Button
+                    variant="primary"
+                    as={Link}
+                    to={routes.signUpPagePath()}
+                    className={`${classes.btnPrimary}`}
+                  >
+                    {t('navbar.signUp')}
+                  </Button>
+                </Nav.Item>
+              )}
+            </Nav>
+          </NavigationBar.Collapse>
+        </Container>
+      </NavigationBar>
+    </header>
   );
 }
