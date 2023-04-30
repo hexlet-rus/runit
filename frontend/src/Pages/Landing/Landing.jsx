771b8516e206af4019f59ffbef361532b837f5f1
Masha <nasonkina@yandex.ru>
2023-05-01 01:26:53 +0300

    fix Landing styles (margins between sections)

--- frontend/src/Pages/Landing/Landing.jsx
+++ frontend/src/Pages/Landing/Landing.jsx
@@ -20,86 +20,74 @@ export function Landing() {
     <div className="min-vh-100 d-flex flex-column bg-dark text-white fw-normal">
       <main className="container pb-5">
         <section className="d-flex flex-column flex-grow h-100 py-3 py-md-4 py-lg-5 justify-content-evenly">
-          <section>
-            <Row>
-              <p className={`text-center mb-0 ${classes.fs5} ${classes.lh5}`}>
-                {t('landing.freeProject')}
-              </p>
-            </Row>
-            <Row className="my-3">
-              <h1
-                className={`${classes.colorPrimary} text-center fw-bold mb-0 mt-sm-5 display-1`}
-              >
-                {t('landing.fastIDE')}
-              </h1>
-            </Row>
-            <Row>
-              <p
-                className={`text-center mb-0 mb-5 mt-sm-4 ${classes.fw500} ${classes.fs3} ${classes.lh3}`}
-              >
-                {t('landing.writeInBrowser')}
-              </p>
-            </Row>
-          </section>
-          <section>
-            <Row className="row-cols-1 row-cols-sm-3">
-              <Col className="d-flex flex-sm-column align-items-center align-items-sm-start mb-3 mb-sm-0">
-                <img
-                  className="mb-sm-3 mb-1 me-3 me-sm-0"
-                  src={tagImg}
-                  alt="tag"
-                />
-                <p className={`${classes.fs5} mb-0 ${classes.lh5}`}>
-                  {t('landing.easyStart.first')}
-                  <br />
-                  {t('landing.easyStart.second')}
-                </p>
-              </Col>
-              <Col className="d-flex flex-sm-column align-items-center align-items-sm-start mb-3 mb-sm-0">
-                <img
-                  className="mb-sm-3 mb-1 me-3 me-sm-0"
-                  src={osImg}
-                  alt="os"
-                />
-                <p className={`${classes.fs5} mb-0 ${classes.lh5}`}>
-                  {t('landing.allDevices.first')}
-                  <br />
-                  {t('landing.allDevices.second')}
-                </p>
-              </Col>
-              <Col className="d-flex flex-sm-column align-items-center align-items-sm-start mb-3 mb-sm-0">
-                <img
-                  className="mb-sm-3 mb-1 me-3 me-sm-0"
-                  src={shareImg}
-                  alt="share"
-                />
-                <p className={`${classes.fs5} mb-0 ${classes.lh5}`}>
-                  {t('landing.shareCode.first')}
-                  <br />
-                  {t('landing.shareCode.second')}
-                </p>
-              </Col>
-            </Row>
-          </section>
-          <section>
-            <Row className="justify-content-center">
-              <Button
-                size="lg"
-                as={Link}
-                to={routes.loginPagePath()}
-                className={`btn ${classes.btnPrimary} ${classes.btnAttention} py-3 ${classes.fs5} fw-bold ${classes.mt65} ${classes.lh5}"`}
-              >
-                {t('landing.startCoding')}
-              </Button>
-            </Row>
-          </section>
-        </section>
-        <section className="mt-5">
           <Row>
-            <h2 className="text-center display-4 fw-bold mb-0">
-              {t('landing.noConfig')}
-            </h2>
+            <p className={`text-center mb-0 ${classes.fs5} ${classes.lh5}`}>
+              {t('landing.freeProject')}
+            </p>
           </Row>
+          <Row className="my-3">
+            <h1
+              className={`${classes.colorPrimary} text-center fw-bold mb-0 mt-sm-5 display-1`}
+            >
+              {t('landing.fastIDE')}
+            </h1>
+          </Row>
+          <Row>
+            <p
+              className={`text-center mb-0 mb-5 mt-sm-4 ${classes.fw500} ${classes.fs3} ${classes.lh3}`}
+            >
+              {t('landing.writeInBrowser')}
+            </p>
+          </Row>
+          <Row className="row-cols-1 row-cols-sm-3">
+            <Col className="d-flex flex-sm-column align-items-center align-items-sm-start mb-3 mb-sm-0">
+              <img
+                className="mb-sm-3 mb-1 me-3 me-sm-0"
+                src={tagImg}
+                alt="tag"
+              />
+              <p className={`${classes.fs5} mb-0 ${classes.lh5}`}>
+                {t('landing.easyStart.first')}
+                <br />
+                {t('landing.easyStart.second')}
+              </p>
+            </Col>
+            <Col className="d-flex flex-sm-column align-items-center align-items-sm-start mb-3 mb-sm-0">
+              <img className="mb-sm-3 mb-1 me-3 me-sm-0" src={osImg} alt="os" />
+              <p className={`${classes.fs5} mb-0 ${classes.lh5}`}>
+                {t('landing.allDevices.first')}
+                <br />
+                {t('landing.allDevices.second')}
+              </p>
+            </Col>
+            <Col className="d-flex flex-sm-column align-items-center align-items-sm-start mb-3 mb-sm-0">
+              <img
+                className="mb-sm-3 mb-1 me-3 me-sm-0"
+                src={shareImg}
+                alt="share"
+              />
+              <p className={`${classes.fs5} mb-0 ${classes.lh5}`}>
+                {t('landing.shareCode.first')}
+                <br />
+                {t('landing.shareCode.second')}
+              </p>
+            </Col>
+          </Row>
+          <Row className="justify-content-center">
+            <Button
+              size="lg"
+              as={Link}
+              to={routes.loginPagePath()}
+              className={`btn ${classes.btnPrimary} ${classes.btnAttention} py-3 ${classes.fs5} fw-bold ${classes.mt65} ${classes.lh5}"`}
+            >
+              {t('landing.startCoding')}
+            </Button>
+          </Row>
+        </section>
+        <section className={`${classes.mt45}`}>
+          <h2 className="text-center display-4 fw-bold mb-0">
+            {t('landing.noConfig')}
+          </h2>
           <Row className="mt-5 row-cols-1 row-cols-sm-2 d-flex justify-content-between">
             <Col className="mb-5 mb-sm-0">
               <p className={`${classes.fs4} mb-0 ${classes.lh4} me-4`}>
@@ -144,86 +132,80 @@ export function Landing() {
             </Col>
           </Row>
         </section>
-        <section className={`${classes.mt90}`}>
-          <Row>
-            <h2 className="text-center display-4 fw-bold mb-0">
-              {t('landing.moreOpportunity')}
-            </h2>
+        <section className={`${classes.mt65}`}>
+          <h2 className="text-center display-4 fw-bold mb-0">
+            {t('landing.moreOpportunity')}
+          </h2>
+          <Row className={`${classes.mt55} row-cols-1 row-cols-sm-3`}>
+            <Col className="d-flex flex-column mb-4 mb-sm-0">
+              <div className="d-flex flex-sm-column align-items-center align-items-sm-start">
+                <img
+                  src={codeImg}
+                  alt="code"
+                  className="mb-3 me-3 me-sm-0"
+                  width="39"
+                  height="39"
+                />
+                <h5 className={`${classes.lh4} ${classes.fs4} mb-2`}>
+                  {t('landing.allLanguages.title')}
+                </h5>
+              </div>
+              <p className={`${classes.lh6} mb-0`}>
+                {t('landing.allLanguages.text')}
+              </p>
+            </Col>
+            <Col className="d-flex flex-column mb-4 mb-sm-0">
+              <div className="d-flex flex-sm-column align-items-center align-items-sm-start">
+                <img
+                  src={personImg}
+                  className="mb-3 me-3 me-sm-0"
+                  alt="person"
+                  width="39"
+                  height="39"
+                />
+                <h5 className={`${classes.lh4} ${classes.fs4} mb-2`}>
+                  {t('landing.teamWork.title')}
+                </h5>
+              </div>
+              <p className={`${classes.lh6} mb-0`}>
+                {t('landing.teamWork.text')}
+              </p>
+            </Col>
+            <Col className="d-flex flex-column mb-4 mb-sm-0">
+              <div className="d-flex flex-sm-column align-items-center align-items-sm-start">
+                <img
+                  src={blankImg}
+                  alt="blank"
+                  className="mb-3 me-3 me-sm-0"
+                  width="39"
+                  height="39"
+                />
+                <h5 className={`${classes.lh4} ${classes.fs4} mb-2`}>
+                  {t('landing.readyBoilerplates.title')}
+                </h5>
+              </div>
+              <p className={`${classes.lh6} mb-0`}>
+                {t('landing.readyBoilerplates.text')}
+              </p>
+            </Col>
           </Row>
-          <section className={`${classes.mt55}`}>
-            <Row className="row-cols-1 row-cols-sm-3">
-              <Col className="mb-4 mb-sm-0">
-                <div className="d-flex flex-column">
-                  <img
-                    src={codeImg}
-                    alt="code"
-                    className="mb-3"
-                    width="39"
-                    height="39"
-                  />
-                  <h5 className={`${classes.lh4} ${classes.fs4} mb-2`}>
-                    {t('landing.allLanguages.title')}
-                  </h5>
-                  <p className={`${classes.lh6} mb-0`}>
-                    {t('landing.allLanguages.text')}
-                  </p>
-                </div>
-              </Col>
-              <Col className="mb-4 mb-sm-0">
-                <div className="d-flex flex-column">
-                  <img
-                    src={personImg}
-                    className="mb-3"
-                    alt="person"
-                    width="39"
-                    height="39"
-                  />
-                  <h5 className={`${classes.lh4} ${classes.fs4} mb-2`}>
-                    {t('landing.teamWork.title')}
-                  </h5>
-                  <p className={`${classes.lh6} mb-0`}>
-                    {t('landing.teamWork.text')}
-                  </p>
-                </div>
-              </Col>
-              <Col className="mb-4 mb-sm-0">
-                <div className="d-flex flex-column">
-                  <img
-                    src={blankImg}
-                    alt="blank"
-                    className="mb-3"
-                    width="39"
-                    height="39"
-                  />
-                  <h5 className={`${classes.lh4} ${classes.fs4} mb-2`}>
-                    {t('landing.readyBoilerplates.title')}
-                  </h5>
-                  <p className={`${classes.lh6} mb-0`}>
-                    {t('landing.readyBoilerplates.text')}
-                  </p>
-                </div>
-              </Col>
-            </Row>
-          </section>
         </section>
         <section className={`${classes.mt45}`}>
-          <Row>
-            <h3 className={`text-center ${classes.fs2} mb-0 ${classes.lh2}`}>
-              {t('landing.nowCoding')}
-            </h3>
-            <Row className="justify-content-center">
-              <Button
-                size="lg"
-                as={Link}
-                to={routes.loginPagePath()}
-                className={`btn ${classes.btnPrimary} ${classes.btnAttention} py-3 ${classes.fs5} fw-bold ${classes.mt40} ${classes.lh5}"`}
-              >
-                {t('landing.startCoding')}
-              </Button>
-            </Row>
+          <h3 className={`text-center ${classes.fs2} mb-0 ${classes.lh2}`}>
+            {t('landing.nowCoding')}
+          </h3>
+          <Row className="justify-content-center">
+            <Button
+              size="lg"
+              as={Link}
+              to={routes.loginPagePath()}
+              className={`btn ${classes.btnPrimary} ${classes.btnAttention} py-3 ${classes.fs5} fw-bold ${classes.mt40} ${classes.lh5}"`}
+            >
+              {t('landing.startCoding')}
+            </Button>
           </Row>
         </section>
-        <section>
+        <section className={`${classes.mt90}`}>
           <Faq />
         </section>
       </main>
