4e13037938675260cde7c0cfa77415af7baf1f57
Max <ppmax@bk.ru>
2023-05-23 16:34:11 +0300

    Profile routes added

--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -26,7 +26,6 @@ function ProtectedRoute({ user, children }) {
 function AuthRoute({ user, children }) {
   if (user) {
     return <Navigate to={routes.defaultProfilePagePath()} replace />;
-    return <Navigate to={routes.defaultProfilePagePath()} replace />;
   }
   return children || <Outlet />;
 }
@@ -43,6 +42,18 @@ function AppRoutes() {
           <Route path={routes.snippetPagePath()} element={<App />} />
           <Route path={routes.aboutPagePath()} element={<About />} />
           <Route element={<ProtectedRoute user={isLoggedIn} />}>
+          <Route
+              path={routes.profilePagePath()}
+              element={<Profile />}
+            />
+            <Route
+              path={routes.defaultProfilePagePath()}
+              element={<Profile />}
+            />
+            <Route
+              path={routes.profileSettingsPagePath()}
+              element={<Profile />}
+            />
           <Route
               path={routes.profilePagePath()}
               element={<Profile />}
