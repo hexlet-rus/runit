cd5029ea9158d9bdf20d7ef514812a2dc8e81f23
Max <ppmax@bk.ru>
2023-05-23 05:41:06 +0300

    Routes change and addition

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -20,7 +20,8 @@ export default {
   getDefaultSnippetName: () => [apiPath, 'snippets', 'name'].join('/'), // get random initial snippet name
   homePagePath: () => '/editor',
   aboutPagePath: () => '/about',
-  profilePagePath: () => '/profile',
+  defaultProfilePagePath: () => '/profile/snippets',
+  profileSettingsPagePath: () => '/profile/settings',
   loginPagePath: () => '/login',
   signUpPagePath: () => '/signup',
   remindPassPagePath: () => '/remind_password',
