52299f0d5c9f353fbe781ffcf441064780922be8
ola-9 <nepogodda@gmail.com>
2023-06-03 20:09:04 +0300

    fix playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -88,7 +88,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start:frontend',
+    command: 'npm run start',
     port: 3000,
     reuseExistingServer: !process.env.CI,
   },
