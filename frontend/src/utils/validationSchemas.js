6168214771728cf834a6bcbc00671b851d3a409e
Dmitry Zhigulin <jigulin.d@gmail.com>
2023-07-04 15:48:26 +0400

    Delete unnecessary translator function call

--- frontend/src/utils/validationSchemas.js
+++ frontend/src/utils/validationSchemas.js
@@ -1,38 +1,37 @@
-import { t } from 'i18next';
 import { string } from 'yup';
 
 export const login = () =>
   string()
     .trim()
-    .required(t('errors.validation.requiredField'))
-    .min(3, t('errors.validation.usernameLength'))
-    .max(16, t('errors.validation.usernameLength'))
-    .matches(/^[\w\S]*$/, t('errors.validation.correctUsername'));
+    .required('errors.validation.requiredField')
+    .min(3, 'errors.validation.usernameLength')
+    .max(16, 'errors.validation.usernameLength')
+    .matches(/^[\w\S]*$/, 'errors.validation.correctUsername');
 
 export const email = () =>
   string()
     .trim()
-    .required(t('errors.validation.requiredField'))
-    .email(t('errors.validation.correctEmail'));
+    .required('errors.validation.requiredField')
+    .email('errors.validation.correctEmail');
 
 export const password = () =>
   string()
     .trim()
-    .required(t('errors.validation.correctEmail'))
-    .min(8, t('errors.validation.passwordLength'))
-    .max(30, t('errors.validation.passwordLength'));
+    .required('errors.validation.correctEmail')
+    .min(8, 'errors.validation.passwordLength')
+    .max(30, 'errors.validation.passwordLength');
 
 export const confirmPassword = () =>
   string()
     .trim()
     .test(
       'confirmPassword',
-      t('errors.validation.confirmPassword'),
+      'errors.validation.confirmPassword',
       (value, context) => value === context.parent.password,
     );
 
 export const replName = () =>
   string()
-    .required(t('errors.validation.required'))
-    .max(20, t('errors.validation.snippetNameMaxLength'))
-    .matches(/^[a-zA-Z0-9_-]*$/, t('errors.validation.singleWord'));
+    .required('errors.validation.requiredField')
+    .max(20, 'errors.validation.snippetNameMaxLength')
+    .matches(/^[a-zA-Z0-9_-]*$/, 'errors.validation.singleWord');
