94a336d8f8c9b777f72f28815d47c6dae9af0b9f
Dmitry Zhigulin <104712148+faciledictu@users.noreply.github.com>
2023-08-03 21:02:05 +0400

    Add DB migration

--- backend/src/config/data-source.config.ts
+++ backend/src/config/data-source.config.ts
@@ -6,6 +6,7 @@ import { migration1663236009774 } from '../migrations/1663236009774-migration';
 import { migration1670352324202 } from '../migrations/1670352324202-migration';
 import { AdduserRecoverHash1677580680097 } from '../migrations/1677580680097-AdduserRecoverHash';
 import { FillNullSlugsSnippets1682678760453 } from '../migrations/1682678760453-fill-null-slugs-snippets';
+import { RenameLoginToUsername1691073864288 } from '../migrations/1691073864288-RenameLoginToUsername';
 
 export default (): DataSourceOptions => {
   config();
@@ -26,6 +27,7 @@ export default (): DataSourceOptions => {
           migration1670352324202,
           AdduserRecoverHash1677580680097,
           FillNullSlugsSnippets1682678760453,
+          RenameLoginToUsername1691073864288,
         ],
       };
     case 'test':
