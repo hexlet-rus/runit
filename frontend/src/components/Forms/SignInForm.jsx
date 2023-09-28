50178193a2c0bc23f883efd6bdfd46ef2a6eb157
elenashcherbinina <el.shcherbinina@outlook.com>
2023-09-28 15:41:31 +0300

    [#381] added link to forgot Password in Forms/SignInForm.jsx

--- frontend/src/components/Forms/SignInForm.jsx
+++ frontend/src/components/Forms/SignInForm.jsx
@@ -146,14 +146,12 @@ function SignInForm({ onSuccess = () => null }) {
           </Form.Group>
         </div>
         <div className="d-flex flex-row gap-5">
-          {formState.message === 'errors.signInFailed' ? (
-            <Link
-              className="icon-link link-secondary d-block align-self-center"
-              to={routes.forgotPassPagePath()}
-            >
-              {t('signIn.remindPass')}
-            </Link>
-          ) : null}
+          <Link
+            className="icon-link link-secondary d-block align-self-center"
+            to={routes.forgotPassPagePath()}
+          >
+            {t('signIn.remindPass')}
+          </Link>
           <Button
             className="flex-fill"
             data-disable-with={t('signIn.signInButton')}
