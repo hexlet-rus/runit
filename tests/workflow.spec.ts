6bae77fd1cb3b92f51323040726c2f2cf9836cf1
d13ch <dm.a.chernov@gmail.com>
2023-10-24 14:03:42 +0300

    add test for html snippet

--- tests/workflow.spec.ts
+++ tests/workflow.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 /* TODO: Тесты каждый раз создают пользователя в приложении,
 все должно происходит в тестовом окружении */
-test('work', async ({ page }) => {
+test('Add and check JavaScript snippet', async ({ page }) => {
   const randomNum = Math.round(Math.random() * 1000 + Math.random() * 100);
   await page.goto('http://localhost:3000');
   await page.getByRole('button', { name: 'Регистрация' }).click();
@@ -22,6 +22,28 @@ test('work', async ({ page }) => {
   await page.getByRole('button', { name: 'Запустить' }).click();
 
   await expect(page.getByText('Hello', { exact: true })).toBeVisible();
+});
+
+test('Add and check HTML snippet', async ({ page }) => {
+  const randomNum = Math.round(Math.random() * 1000 + Math.random() * 100);
+  await page.goto('http://localhost:3000');
+  await page.getByRole('button', { name: 'Регистрация' }).click();
+  await page.getByLabel('Электронная почта').fill(`test${randomNum}@test.test`);
+  await page.getByLabel('Имя пользователя').fill(`test${randomNum}`);
+  await page.getByLabel('Пароль', { exact: true }).fill('12345678');
+  await page.getByRole('button', { name: 'Зарегистрироваться' }).click();
+  await page
+    .getByRole('button', { name: 'Создать сниппет на HTML' })
+    .click();
+
+  await page
+    .getByRole('textbox', {
+      name: 'Editor content;Press Alt+F1 for Accessibility Options.',
+    })
+    .fill('<!--Здесь будет код на HTML-->\n<div style="background-color: red">Hello, World!</div>');
+
+  const locator = page.frameLocator('iframe[title="render"]').getByText('Hello, World!', { exact: true });
+  await expect(locator).toHaveCSS('background-color', 'rgb(255, 0, 0)');
 });
 
 test('Unable to register by invalid email ', async ({ page }) => {
