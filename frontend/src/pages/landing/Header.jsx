35ed2b7b8ae3ac8716c64d78f837fb86c3eaf1c1
Daniil Bobrov <allodrmen@yandex.ru>
2024-04-09 13:16:23 +0300

    back to left items inside collapse header

--- frontend/src/pages/landing/Header.jsx
+++ frontend/src/pages/landing/Header.jsx
@@ -34,7 +34,7 @@ function Header() {
             <Image src={Burger} />
           </Navbar.Toggle>
           <Navbar.Collapse className="my-3 mb-lg-0" id="navbar-responsive">
-            <Nav as="ul" className="text-center">
+            <Nav as="ul" className="text-left text-xl-center">
               <li>
                 <Navbar.Brand className="header-link" href="#aboutProject">
                   <span>{t('landing.header.about')}</span>
@@ -58,7 +58,7 @@ function Header() {
             </Nav>
             <Nav
               as="ul"
-              className="mb-3 mb-lg-0 gap-1 ms-auto align-items-center"
+              className="mb-3 mb-lg-0 gap-1 ms-auto align-items-xl-center align-items-start"
             >
               <LanguageSelector />
               <ThemeSelector />
