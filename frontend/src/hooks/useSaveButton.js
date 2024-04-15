7b78162606cda167f9665a95eab2155cd1a35d63
Daniil Bobrov <allodrmen@yandex.ru>
2024-04-15 18:04:21 +0300

    removeNavigation

--- frontend/src/hooks/useSaveButton.js
+++ frontend/src/hooks/useSaveButton.js
@@ -3,17 +3,14 @@
 
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import routes from '../routes.js';
 import { actions } from '../slices/editorSlice.js';
 
 const useSaveButton = () => {
   const currentCode = useSelector((state) => state.editor.code);
-  const username = useSelector((state) => state.user.userInfo.username);
   const dispatch = useDispatch();
-  const navigate = useNavigate();
   const saveCode = () => {
     dispatch(actions.setCodeAndSavedCode(currentCode));
-    navigate(routes.profilePagePath(username));
+    
   };
 
   return {
