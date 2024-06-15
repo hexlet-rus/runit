3a33e39c3741219da011ed643b4d97785eebebf5
VictoryPashkova <victory.pashkova@gmail.com>
2024-06-15 22:09:48 +0600

    change path for translator in Loaders

--- frontend/src/components/Loaders/DefaultLoader.jsx
+++ frontend/src/components/Loaders/DefaultLoader.jsx
@@ -1,14 +1,14 @@
 import { useTranslation } from 'react-i18next';
 
 function DefaultLoader() {
-  const { t } = useTranslation();
+  const { t: tAS } = useTranslation('translations', { keyPrefix: 'appStatus' });
 
   return (
     <div className="h-100 m-auto d-flex flex-column justify-content-center align-items-center text-secondary">
       <div className="spinner-border" role="status">
-        <span className="visually-hidden">{t('appStatus.loading')}</span>
+        <span className="visually-hidden">{tAS('loading')}</span>
       </div>
-      <p className="pt-3">{t('appStatus.loading')}</p>
+      <p className="pt-3">{tAS('loading')}</p>
     </div>
   );
 }
