bf682f9d612b7d0eca50e803dbd414550b8906f7
Vyacheslav Pilyugin <vyach-pvv@yandex.ru>
2024-07-05 01:05:54 +0700

    Feat add new component SnippetCheck

---
+++ frontend/src/pages/profile/SnippetCheck.jsx
@@ -0,0 +1,38 @@
+import { Check2Square, XLg } from 'react-bootstrap-icons';
+import { useTranslation } from 'react-i18next';
+import Button from 'react-bootstrap/Button';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { actions as snippetsActions } from '../../slices/snippetsSlice.js';
+
+function SnippetCheck() {
+  const { isCheckboxesOpen } = useSelector((state) => state.snippets);
+  const { t } = useTranslation();
+  const { snippets } = useSelector((state) => state.snippets);
+  const isCreateSnippet = snippets.length > 0;
+  const dispatch = useDispatch();
+
+  const handleCloseCheckboxes = () => {
+    dispatch(snippetsActions.CloseCheckboxes());
+  };
+
+  const handleOpenCheckboxes = () => {
+    dispatch(snippetsActions.OpenCheckboxes());
+  };
+
+  return (
+    <div className={`${isCreateSnippet ? '' : 'd-none'}`}>
+      {isCheckboxesOpen ? (
+        <Button variant="outline-primary" onClick={handleCloseCheckboxes}>
+          {t('snippetActions.cancelButton')} <XLg />
+        </Button>
+      ) : (
+        <Button variant="outline-primary" onClick={handleOpenCheckboxes}>
+          {t('snippetActions.select')} <Check2Square />
+        </Button>
+      )}
+    </div>
+  );
+}
+
+export default SnippetCheck;
