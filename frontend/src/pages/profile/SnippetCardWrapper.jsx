bfce1eda4c42d7862c7e54b6108b22064ac39cd3
Serge Tsepodoy <sp1tfire_90@mail.ru>
2024-09-25 16:11:24 +0500

    feat[hexlet-rus#542]: add tooltip on snippet wrapper card

--- frontend/src/pages/profile/SnippetCardWrapper.jsx
+++ frontend/src/pages/profile/SnippetCardWrapper.jsx
@@ -1,5 +1,13 @@
 function SnippetCardWrapper({ children }) {
-  return <div className="snippet-card-wrapper">{children}</div>;
+  const hoverTooltip =
+    children.props.children.length > 1
+      ? children.props.children[0].props.data?.name
+      : '';
+  return (
+    <div className="snippet-card-wrapper" title={hoverTooltip}>
+      {children}
+    </div>
+  );
 }
 
 export default SnippetCardWrapper;
