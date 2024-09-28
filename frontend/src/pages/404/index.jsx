ff7de4cffe652c54b9aa19094ed8c4ed63c13da1
Serge Tsepodoy <sp1tfire_90@mail.ru>
2024-09-28 15:43:56 +0500

    fix[hexlet-rus#556]: fix text visibility in Dark mode on 404 page

--- frontend/src/pages/404/index.jsx
+++ frontend/src/pages/404/index.jsx
@@ -1,10 +1,13 @@
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
+import { useTernaryDarkMode } from 'usehooks-ts';
 
 function NotFound() {
   const { t: tPNF } = useTranslation('translation', {
     keyPrefix: 'pageNotFound',
   });
+  const { isDarkMode } = useTernaryDarkMode();
+  const paragraphClass = `fs-5 mb-3 ${isDarkMode ? '' : 'text-black '}`;
 
   return (
     <div className="container mb-5">
@@ -19,11 +22,11 @@ function NotFound() {
         </div>
         <div className="col">
           <h1 className="mb-4">{tPNF('title')}</h1>
-          <p className="fs-5 text-black mb-3">{tPNF('whatHappened.title')}</p>
+          <p className={paragraphClass}>{tPNF('whatHappened.title')}</p>
           <p className="mb-4">{tPNF('whatHappened.body')}</p>
-          <p className="fs-5 text-black mb-3">{tPNF('whyHappened.title')}</p>
+          <p className={paragraphClass}>{tPNF('whyHappened.title')}</p>
           <p className="mb-4">{tPNF('whyHappened.body')}</p>
-          <p className="fs-5 text-black mb-3">{tPNF('whatToDo.title')}</p>
+          <p className={paragraphClass}>{tPNF('whatToDo.title')}</p>
           <p className="mb-2">
             {tPNF('whatToDo.body')}
             <Link to="/">{tPNF('whatToDo.returnButton')}</Link>
