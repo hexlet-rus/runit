695db32bdae79ec837c7892a5bf838bef88968d0
Serge Tsepodoy <sp1tfire_90@mail.ru>
2024-10-06 12:19:57 +0500

    fix[hexlet-rus#556]: delete unnecessary file and fix BS classes in accordance with @fey remarks

--- frontend/src/pages/404/index.jsx
+++ frontend/src/pages/404/index.jsx
@@ -1,13 +1,10 @@
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
-import './404.css';
 
 function NotFound() {
   const { t: tPNF } = useTranslation('translation', {
     keyPrefix: 'pageNotFound',
   });
-
-  const paragraphClass = `fs-5 mb-3 paragraph-color`;
 
   return (
     <div className="container mb-5">
@@ -22,11 +19,11 @@ function NotFound() {
         </div>
         <div className="col">
           <h1 className="mb-4">{tPNF('title')}</h1>
-          <p className={paragraphClass}>{tPNF('whatHappened.title')}</p>
+          <p className="fs-5 mb-3">{tPNF('whatHappened.title')}</p>
           <p className="mb-4">{tPNF('whatHappened.body')}</p>
-          <p className={paragraphClass}>{tPNF('whyHappened.title')}</p>
+          <p className="fs-5 mb-3">{tPNF('whyHappened.title')}</p>
           <p className="mb-4">{tPNF('whyHappened.body')}</p>
-          <p className={paragraphClass}>{tPNF('whatToDo.title')}</p>
+          <p className="fs-5 mb-3">{tPNF('whatToDo.title')}</p>
           <p className="mb-2">
             {tPNF('whatToDo.body')}
             <Link to="/">{tPNF('whatToDo.returnButton')}</Link>
