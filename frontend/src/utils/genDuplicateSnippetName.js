0ed379868497ce4f397e659ba3c24c0ea9378021
Serge Tsepodoy <sp1tfire_90@mail.ru>
2024-10-22 17:19:51 +0500

    refactor[hexlet-rus#542]: refactor nested conditions

--- frontend/src/utils/genDuplicateSnippetName.js
+++ frontend/src/utils/genDuplicateSnippetName.js
@@ -9,18 +9,17 @@ const genDuplicateSnippetName = (fileName) => {
 
   let finalName = '';
   const extension = extensionIndex !== -1 ? fileName.slice(extensionIndex) : '';
+  const conditionalFileName =
+    extensionIndex !== -1 ? fileName.slice(0, extensionIndex) : fileName;
+  const isAlreadyCopy = copyTextIndex !== -1;
+  const increaseCopy = `${fileName.slice(0, dashIndex)}_${existCopyNumber + 1}`;
 
-  if (copyTextIndex !== -1) {
-    if (dashIndex > copyTextIndex) {
-      finalName = `${fileName.slice(0, dashIndex)}_${
-        existCopyNumber + 1
-      }${extension}`;
-    } else if (extensionIndex !== -1) {
-      finalName = `${fileName.slice(0, extensionIndex)}_${1}${extension}`;
-    } else {
-      finalName = `${fileName}_${1}${extension}`;
-    }
-  } else finalName = `${fileName}-copy${extension}`;
+  if (isAlreadyCopy && dashIndex > copyTextIndex) {
+    finalName = `${increaseCopy}${extension}`;
+  } else {
+    finalName = `${conditionalFileName}_${1}${extension}`;
+  }
+  if (!isAlreadyCopy) finalName = `${conditionalFileName}-copy${extension}`;
 
   if (finalName.length >= SNIPPET_NAME_MAX_LENGTH) {
     return finalName.slice(finalName.length - SNIPPET_NAME_MAX_LENGTH);
