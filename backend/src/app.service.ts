9b5de024f7e6ad5310000a79990f166d7d7a78b1
Ivan Gagarinov <dzencot@gmail.com>
2024-10-30 16:56:40 +0500

    fix lint errors

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,15 +1,14 @@
 import * as vm from 'node:vm';
 import { Console } from 'node:console';
 import { Transform } from 'node:stream';
+import { execSync } from 'node:child_process';
 import { Injectable } from '@nestjs/common';
 import { createContext } from './console/console.config';
 import { Output } from './console/interfaces/output.interface';
-import { execSync } from 'node:child_process';
 
 @Injectable()
 export class AppService {
   // eslint-disable-next-line class-methods-use-this
-
   async run(code: string, language: string): Promise<Output> {
     if (language === 'html') {
       return { terminal: [code], alertLogs: [] };
@@ -67,6 +66,7 @@ export class AppService {
 
       return { terminal, alertLogs };
     } catch (err) {
+      // eslint-disable-next-line no-console
       console.log(err.message);
       const lineOfError = err.stack
         .split('evalmachine.<anonymous>:')[1]
