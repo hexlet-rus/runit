880c92e573bc29e470fd7a36b6b7acb6bb95899e
Sidorov Vladimir <v.sidorov29091988@gmail.com>
2024-12-29 18:12:55 +0700

    Update HtmlRunner.ts
    fix lint

--- backend/src/runner/HtmlRunner.ts
+++ backend/src/runner/HtmlRunner.ts
@@ -1,7 +1,6 @@
 import IRunner from './IRunner';
 
 export default class HtmlRunner implements IRunner {
-
   // eslint-disable-next-line class-methods-use-this
   run(code: string) {
     return Promise.resolve({ terminal: [code], alertLogs: [] });
