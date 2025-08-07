bf8d3773205b864ee76031958bf48da5c12efd02
dev <legendaryje@gmail.com>
2025-08-08 00:13:04 +0300

    slices on TS

--- frontend/src/slices/index.ts
+++ frontend/src/slices/index.ts
@@ -1,23 +1,24 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import editorReducer, { actions as editorActions } from './editorSlice.js';
+import editorReducer, { actions as editorActions } from './editorSlice';
 import terminalReducer, {
   actions as terminalActions,
   runCode,
 } from './terminalSlice.js';
 import languagesReducer, {
   actions as languagesActions,
-} from './languagesSlice.js';
-import modalReducer, { actions as modalActions } from './modalSlice.js';
+} from './languagesSlice';
+import modalReducer, { actions as modalActions } from './modalSlice';
 import snippetsSlice, { actions as snippetsActions } from './snippetsSlice.js';
 import userSlice, { actions as userActions } from './userSlice';
 import checkboxesSlice, {
   actions as checkboxesActions,
-} from './checkboxesSlice.js';
+} from './checkboxesSlice';
 import userSettingsSlice, {
   actions as userSettingsActions,
 } from './userSettingsSlice';
+import { RootReducerType } from 'src/types/state';
 
-export const rootReducer = combineReducers({
+export const rootReducer = combineReducers<RootReducerType>({
   editor: editorReducer,
   terminal: terminalReducer,
   languages: languagesReducer,
