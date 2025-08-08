826c6d68014880ea87741e1c01ffe5f07c1c4acf
fairwind2k <fairwind@md2k.net>
2025-08-08 17:11:46 +0200

    update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  schema: './src/db/schema.ts',
+  schema: './src/db/schema/*',
   out: './drizzle',
   dialect: 'sqlite',
   dbCredentials: {
-    url: './data/database.sqlite',
+    url: 'database.sqlite',
   },
   verbose: true,
   strict: true,
-  breakpoints: false,
+  breakpoints: true,
 });
+
+// credentials for postgres:
+// dbCredentials: {
+//     url: "postgres://user:password@host:port/db",
+//   }
