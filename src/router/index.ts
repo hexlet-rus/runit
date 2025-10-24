db2b46f708b7661e62563ca32da70704a402d370
fairwind2k <fairwind@md2k.net>
2025-10-25 00:37:37 +0200

    add test router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { router } from '../context';
 import { userRouter } from './userRouter';
-import { snippetRouter } from './snippetRouter'; // Импортируем роутер сниппетов
+import { snippetRouter } from './snippetRouter';
+import { testDataRouter } from './testDataRouter';
 
 export const appRouter = router({
   users: userRouter,    // Роутер для пользователей
-  snippets: snippetRouter, // Добавляем роутер для сниппетов
+  snippets: snippetRouter, // роутер для сниппетов
+  testData: testDataRouter // роутер для проверки работоспособности БД
 });
 
 export type AppRouter = typeof appRouter;
