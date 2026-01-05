b0504d0e086b923ab8c85470667ade213a9fc9a5
DmitryBalandin <balandinqwerty1983@gmail.com>
2026-01-05 22:01:59 +0500

    feat:header start

--- frontend/src/pages/ProfilePage/Header.tsx
+++ frontend/src/pages/ProfilePage/Header.tsx
@@ -1,10 +1,39 @@
+import {
+    Paper,
+    Title,
+    Button,
+    ActionIcon,
+    Text,
+    Group
+} from '@mantine/core';
+import { ReactComponent as IconLanguage } from './assets/Language.svg';
 
-
-
-const Header = () =>{
-    return (<div>
-        Header
-        </div>)
+const Header = () => {
+    return (
+        <Paper pe='md' >
+            <Group justify="space-between" h={60} wrap='nowrap'>
+                <Group h={60} wrap='nowrap'>
+                    <Text>Icon</Text>
+                    <Title>runit</Title>
+                </Group>
+                <Group h={60} wrap='nowrap'>
+                    <ActionIcon
+                        variant="default"
+                        radius='md'
+                        
+                    >
+                        <IconLanguage  style={{ width: 20, height: 20 }} />
+                    </ActionIcon>
+                    <Button
+                        variant="default"
+                        radius='md'
+                    >
+                        Выйти
+                    </Button>
+                </Group>
+            </Group>
+        </Paper>
+    )
 }
 
 export default Header
