2a67950e8138ea846ffcd89e2a16ef160f95e24b
DmitryBalandin <balandinqwerty1983@gmail.com>
2026-01-10 21:51:52 +0500

    header full

--- frontend/src/pages/ProfilePage/Header.tsx
+++ frontend/src/pages/ProfilePage/Header.tsx
@@ -3,30 +3,46 @@ import {
     Title,
     Button,
     ActionIcon,
-    Text,
-    Group
+    Box,
+    Group,
+    Anchor
 } from '@mantine/core';
+import { Link } from 'react-router-dom';
 import { ReactComponent as IconLanguage } from './assets/Language.svg';
 
 const Header = () => {
     return (
-        <Paper pe='md' >
-            <Group justify="space-between" h={60} wrap='nowrap'>
-                <Group h={60} wrap='nowrap'>
-                    <Text>Icon</Text>
-                    <Title>runit</Title>
-                </Group>
+        <Paper  px='md'>
+            <Group justify="space-between" h={60} wrap='nowrap' align="center">
+                <Anchor
+                    component={Link}
+                    to='/'
+                    underline='never'
+                    style={{ color: 'black' }}
+                >
+                    <Group h={60} wrap='nowrap' align="center">
+                        <Box
+                            w={30}
+                            h={30}
+                            style={{
+                                background: 'linear-gradient(135deg, #3776FB, #4C47F4)',
+                                borderRadius: '10px'
+                            }}
+                        />
+                        <Title order={1} size='h2' pb={6}>runit</Title>
+                    </Group>
+                </Anchor>
                 <Group h={60} wrap='nowrap'>
                     <ActionIcon
                         variant="default"
                         radius='md'
-                        
+                        size={34}
                     >
-                        <IconLanguage  style={{ width: 20, height: 20 }} />
+                        <IconLanguage style={{ width: 20, height: 20 }} />
                     </ActionIcon>
                     <Button
                         variant="default"
-                        radius='md'
+                        radius='lg'
                     >
                         Выйти
                     </Button>
